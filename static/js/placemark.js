cc8a67ad00cfb38c8b4c483ae45b04e2bde8d574
Rick <romeo2310@yandex.ru>
2020-08-31 13:09:29 +0300

    added map into the cont.page

---
+++ static/js/placemark.js
@@ -0,0 +1,32 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.778726, 37.693746],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'ул.Фридриха Энгельса, 75 с.3',
+            balloonContent: 'Спуск по лестнице на минус 1 этаж, 1 офис'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: icon_scr,
+            // Размеры метки.
+            iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+});
